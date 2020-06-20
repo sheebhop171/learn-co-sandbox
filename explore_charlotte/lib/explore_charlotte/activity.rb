bd946b07f60c8ebd2bc8a064360c1acfd6f0faba
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-20 21:25:15 +0000

    scraping work

--- explore_charlotte/lib/explore_charlotte/activity.rb
+++ explore_charlotte/lib/explore_charlotte/activity.rb
@@ -10,7 +10,7 @@ class ExploreCharlotte::Activity
   
   def self.scrape_types
     
-    activites = []
+    activities = []
     
     activities << self.scrape_cga
     
@@ -20,7 +20,14 @@ class ExploreCharlotte::Activity
   def self.scrape_cga
 
     doc = Nokogiri::HTML(open("https://www.charlottesgotalot.com/eat-drink"))
-    
+    #binding.pry
+    results = doc.css(".Card.Card--place").text.strip
+
+   # results.each do |r|
+    # puts r.css(".Card-title").text.strip
+     #puts r.css(".Card-type").text.strip
+    #end 
+
 
   end
   
