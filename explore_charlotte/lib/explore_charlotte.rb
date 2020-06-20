3f60cfdae203fd1f322528586fecad568df26ce2
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-20 16:38:36 +0000

    #stubbing out get_activites & #goodbye

--- explore_charlotte/lib/explore_charlotte.rb
+++ explore_charlotte/lib/explore_charlotte.rb
@@ -2,8 +2,11 @@ require "bundler/setup"
 require "explore_charlotte"
 require_relative "explore_charlotte/version"
 require_relative "explore_charlotte/cli"
+require_relative "explore_charlotte/activity"
+require_relative "explore_charlotte/scraper"
 require 'pry'
 require 'open-uri'
+
 
 module ExploreCharlotte
   class Error < StandardError; end
--- explore_charlotte/lib/explore_charlotte/cli.rb
+++ explore_charlotte/lib/explore_charlotte/cli.rb
@@ -1,9 +1,48 @@
-#My CLI Controller 
+#My CLI Controller - responsible for business logic and user interactions
 
 class ExploreCharlotte::CLI
   
   def call
-    puts "Welcome to Charlotte!"
-    #binding.pry
+    puts "Welcome to Charlotte! Let’s explore some featured activities based on your interests."
+    list_activity_categories
+    get_activities
+    goodbye
+  end 
+  
+  def list_activity_categories
+    puts "What type of activity are you looking to explore?"
+    puts "1. Attractions" 
+    puts "2. Outdoors & Adventure"
+    puts "3. Golf"
+    puts "4. Craft Beer"
+    puts "5. Restaurants"
+  end
+  
+  def get_activities
+    puts "Enter the number of the activity you'd like to explore further or else enter 'exit'."
+    
+    input = nil
+    
+    while input != "exit"
+    
+    input = gets.strip 
+    
+    case input 
+      when "1" 
+        puts "Nascar Hall of Fame"
+      when "2"
+        puts "Whitewater Center"
+      when "3" 
+        puts "The Learning Center"
+      when "4"
+        puts "Birdson Brewing"
+      when "5"
+        puts "Superica"
+      end 
+    end
+  end
+  
+  def goodbye
+    puts "Thanks for visiting Charlotte! See you again next time."
   end 
 end 
