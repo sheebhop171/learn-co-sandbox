2360a273fa392c8d0ac967c3e3cbf982352a0e0e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-10 15:16:13 +0000

    Automatically backed up by Learn

---
+++ explore_charlotte/config/environment.rb
@@ -0,0 +1,9 @@
+require "bundler/setup"
+require "explore_charlotte"
+require_relative "explore_charlotte/version"
+require_relative "explore_charlotte/cli"
+require_relative "explore_charlotte/activity"
+require_relative "explore_charlotte/scraper"
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
