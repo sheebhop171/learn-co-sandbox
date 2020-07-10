3d76476c83771417a81a663c6ab2ecc2d1a8d4e5
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-10 18:01:26 +0000

    test update

---
+++ superhero-cli/lib/superhero/cli.rb
@@ -0,0 +1,8 @@
+require "superhero/cli/version"
+
+module Superhero
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ superhero-cli/lib/superhero/cli/version.rb
@@ -0,0 +1,5 @@
+module Superhero
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
